6b0996cd74b216e63f9a69d97f7bf50c03b7a712
Farkas Benedek <144097374+Benedek44@users.noreply.github.com>
2024-12-16 13:27:26 +0100

    Add files via upload

---
+++ jokes.js
@@ -0,0 +1,32 @@
+//Készítette: Farkas Benedek, Rózas Richárd, Tóthmihály Martin
+let joke;
+let jokeButton = document.getElementById('jokeBtn');
+let newJokeButton = document.getElementById('newJokeBtn');
+let jokeAnswer = document.getElementById('answer');
+let jokeText = document.getElementById('joke');
+
+jokeAnswer.style.display = 'none';
+document.addEventListener('DOMContentLoaded', () => {
+  fetchJoke();
+
+  jokeButton.addEventListener('click', () => {
+    jokeAnswer.innerHTML = joke.punchline;
+    jokeAnswer.style.display = 'block';
+  });
+
+  newJokeButton.addEventListener('click', () => {
+    jokeAnswer.style.display = 'none';
+    fetchJoke();
+  });
+});
+
+function fetchJoke() {
+  fetch('https://official-joke-api.appspot.com/jokes/random')
+    .then(response => response.json())
+    .then(data => {
+      joke = data;
+      joke.innerHTML = "";
+      jokeText.innerHTML = joke.setup;
+    })
+    .catch(error => console.error('Error fetching the joke:', error));
+}
